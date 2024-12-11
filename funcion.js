f33b2268b9c322a0a23e2645ba60bb23a518c2e5
David <drdavialberto5@gmail.com>
2024-12-11 12:45:46 -0600

    html y css

--- funcion.js
+++ funcion.js
@@ -6,3 +6,20 @@ function validarNumeroCambio(){
         inputNumero.value = 0;
     }
 }
+
+function validarCampos(){
+    const inputNumero = document.getElementById("inputNoGuía");
+    let camposValidos = true;
+    if((inputNumero.value < 0) || (inputNumero.value == "")){
+        camposValidos = false;
+    }
+    return camposValidos;
+}
+
+function obtenerInformaciónEnvio(){
+    if(validarCampos()){
+        alert("Buscando información");
+    }else{
+        alert("Error: Número de guía no valido.")
+    }
+}
