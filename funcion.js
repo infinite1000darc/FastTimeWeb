bf9d72fe70176ceaf8e48c7089861513a369a399
David <drdavialberto5@gmail.com>
2024-12-11 13:21:38 -0600

    html y css

--- funcion.js
+++ funcion.js
@@ -20,8 +20,54 @@ function validarCampos(){
 
 function obtenerInformaciónEnvio(){
     if(validarCampos()){
-        alert("Buscando información");
+        obtenerInformacionPaquetes();
     }else{
         alert("Error: Número de guía no valido.")
+    }
+}
+
+function mostrarPaquetes(listaPaquetes, paquetes){
+    listaPaquetes.innerHTML = "";
+    paquetes.forEach(paquete => {
+        const paqueteElemento = document.createElement('div');
+        paqueteElemento.className = "lista-elementos";
+        paqueteElemento.innerHTML = `
+            <strong> Descripcion: <strong> ${paquete.descripcionPaquete} <br>
+            <strong> Alto: <strong> ${paquete.alto} <br>
+            <strong> Ancho: <strong> ${paquete.ancho} <br>
+            <strong> Profundidad: <strong> ${paquete.profundidad} <br>
+            <strong> Peso: <strong> ${paquete.peso} <br>
+        `;
+        listaPaquetes.appendChild(paqueteElemento);
+    });
+}
+
+async function obtenerInformacionPaquetes(){
+    const listaPaquetes = document.getElementById("lista-paquetes");
+    const inputNumero = document.getElementById("inputNoGuía");
+
+    listaPaquetes.innerHTML = "<p>Cargando información de paquetes...</p>";
+
+    const noGuía = inputNumero.value;
+
+    const peticion = URL_OBTENER_PAQUETES+noGuía;
+
+    //Inicia consumo
+    try{
+        const respuesta = await fetch(peticion, {
+            method: 'GET'
+        });
+
+        if(respuesta.ok){
+            const paquetes = await respuesta.json();
+            console.log(paquetes);
+            mostrarPaquetes(listaPaquetes,paquetes);
+        }else{
+            //No es ni acento ni comilla simple, es ` y se activa con alt gr en el }
+            throw new Error(`Error en la petición: ${respuesta.status}`);
+        }
+    }catch(error){
+        console.error("Error en la peticion", error);
+        listaPaquetes.innerHTML = "<p>Hubo un error en la conexión. Intente después.</p>"
     }
 }
